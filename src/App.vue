da15692f5e08b29405a9de2aeae3c775b629b532
Jesus <jesus20102000@gmail.com>
2023-02-12 18:07:45 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/listar">Home</router-link> |
     <router-link to="/login">Login</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
