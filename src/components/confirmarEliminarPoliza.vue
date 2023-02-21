33d87c419f223afa65de3e690cdc1da4f4a612ca
Jesus <jesus20102000@gmail.com>
2023-02-21 21:31:19 +0100

    commit

---
+++ src/components/confirmarEliminarPoliza.vue
@@ -0,0 +1,99 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="card-header">
+                <h3>Estas seguro de eliminar la poliza?</h3>
+            </div>
+            <div class="card-body">
+                <table id="listaPolizas" class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Importe</th>
+                            <th>Fecha de creacion</th>
+                            <th>Estado</th>
+                            <th>Observaciones</th>
+                            <th>ID del cliente</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-bind:class="{ 'bg-success': poliza.estado == 'Cobrada', 'bg-danger': poliza.estado == 'Anulada', 'bg-warning': poliza.estado == 'A cuenta', 'bg-info': poliza.estado == 'Liquidada', 'bg-secondary': poliza.estado == 'Pre-Anulada' }">
+                            <td>{{ poliza.idpoliza }}</td>
+                            <td>{{ poliza.importe }}</td>
+                            <td>{{ poliza.fecha }}</td>
+                            <td>{{ poliza.estado }}</td>
+                            <td>{{ poliza.observaciones }}</td>
+                            <td>{{ poliza.cliente_idcliente }}</td>
+                            <td>
+                                <div class="btn-group" role="group" aria-label="">
+
+                                    <button type="button" v-on:click="borrarPoliza(poliza.idpoliza)"
+                                        class="btn btn-danger">Si</button>
+                                   
+                                    <button type="button" v-on:click="borrarPoliza(poliza.idpoliza)"
+                                        class="btn btn-success">No</button>
+                                    
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import 'jquery/dist/jquery.min.js';
+import "datatables.net-dt/js/dataTables.dataTables"
+import "datatables.net-dt/css/jquery.dataTables.min.css"
+import $ from 'jquery';
+export default {
+    data() {
+
+        return {
+            poliza: {
+
+            }
+        }
+    },
+    created: function () {
+        this.consultarPoliza();
+    },
+    methods: {
+        consultarPoliza() {
+            fetch('http://localhost/poliza/')
+                .then(respuesta => respuesta.json())
+                .then((datosRespuesta) => {
+
+                    if (typeof datosRespuesta[0].success === 'undefined') {
+
+                        this.polizas = datosRespuesta;
+                    }
+                })
+                .catch(console.log)
+        },
+        borrarPoliza(id) {
+            fetch('http://localhost/poliza/?borrar=' + id)
+                .then(respuesta => respuesta.json())
+                .then((datosRespuesta) => {
+
+                    console.log(datosRespuesta)
+                    window.location.href = "listar"
+
+
+                })
+                .catch(console.log)
+        }
+    }
+}
+</script>
+<style>
+
+#listaPolizas th {
+    text-align: center;
+}
+
+</style>
