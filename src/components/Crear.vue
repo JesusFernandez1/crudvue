6a51839c32b509c4056e0b9678b60aaa8a063e5b
Jesus <jesus20102000@gmail.com>
2023-02-21 19:37:16 +0100

    commit

--- src/components/Crear.vue
+++ src/components/Crear.vue
@@ -1,70 +1,71 @@
 <template>
-    <div class="container">
-        <div class="card">
-            <div class="card-header">
-                Nuevo cliente
-            </div>
-            <div class="card-body">
-                
-                <form v-on:submit.prevent="agregarRegistro">
+  <div class="container">
+      <div class="card">
+          <div class="card-header">
+              Nuevo cliente
+          </div>
+          <div class="card-body">
+              
+            <form v-on:submit.prevent="agregarRegistro">
 
-                    <div class="mb-3">
-                      <label for="nombre" class="form-label">Nombre:</label>
-                      <input type="text"
-                        class="form-control" required name="nombre" v-model="cliente.nombre" id="nombre" aria-describedby="helpId" placeholder="Nombre">
-                      <small id="helpId" class="form-text text-muted">Escribe el nombre</small>
-                    </div>
+                <div class="mb-3">
+                  <label for="nombre" class="form-label">Nombre:</label>
+                    <input type="text" class="form-control" required name="nombre" v-model="cliente.nombre" id="nombre" aria-describedby="helpId" placeholder="Nombre">
+                    <small id="helpId" class="form-text text-muted">Escribe el nombre</small>
+                </div>
 
-                    <div class="mb-3">
-                      <label for="apellido" class="form-label">Apellido:</label>
-                      <input type="text"
-                        class="form-control" required name="apellido" v-model="cliente.apellido" id="apellido" aria-describedby="helpId" placeholder="Apellido">
-                      <small id="helpId" class="form-text text-muted">Escribe el apellido</small>
-                    </div>
+                <div class="mb-3">
+                  <label for="apellido" class="form-label">Apellido:</label>
+                  <input type="text" class="form-control" required name="apellido" v-model="cliente.apellido" id="apellido" aria-describedby="helpId" placeholder="Apellido">
+                  <small id="helpId" class="form-text text-muted">Escribe el apellido</small>
+                </div>
 
-                    <div class="mb-3">
-                      <label for="Telefono" class="form-label">Telefono:</label>
-                      <input type="text"
-                        class="form-control" required name="Telefono" v-model="cliente.telefono" id="Telefono" aria-describedby="helpId" placeholder="Telefono">
-                      <small id="helpId" class="form-text text-muted">Escribe el Telefono</small>
-                    </div>
+                <div class="mb-3">
+                  <label for="Telefono" class="form-label">Telefono:</label>
+                  <input type="text" class="form-control" required name="Telefono" v-model="cliente.telefono" id="Telefono" aria-describedby="helpId" placeholder="Telefono">
+                  <small id="helpId" class="form-text text-muted">Escribe el Telefono</small>
+                </div>
 
-                    <div class="mb-3">
-                      <label for="provincia" class="form-label">Provincia:</label>
-                      <select v-model="provinciaTarget" v-on:change="consultarMunicipios(provinciaTarget)" required name="provincia" id="provincia" class="form-select" aria-label="Default select example">
-                        <option disabled selected>Elija una provincia</option>
-                        <option v-for="provincia in provincias" :key="provincia.id" v-bind:value="provincia.id">{{provincia.provincia}}</option>
-                        </select>
-                    </div>
+                  <div class="mb-3">
+                    <label for="provincia">Provincia:</label>
+                    <select v-model="provinciaSeleccionada" required name="provincia" id="provincia" class="form-select" aria-label="Default select example">
+                      <option value="">Selecciona una provincia</option>
+                      <option v-for="provincia in provincias" :key="provincia.id" :value="provincia.id">
+                        {{ provincia.provincia }}
+                      </option>
+                    </select>
+                  </div>
 
-                    <div class="mb-3">
-                      <label for="codigo_postal" class="form-label">Codigo Postal:</label>
-                      <input type="text"
-                        class="form-control" required name="codigo_postal" v-model="cliente.codigo_postal" id="codigo_postal" aria-describedby="helpId" placeholder="Codigo Postal">
-                      <small id="helpId" class="form-text text-muted">Escribe el codgio Postal</small>
-                    </div>
+                  <div class="mb-3">
+                    <label for="codigo_postal" class="form-label">Codigo Postal:</label>
+                    <input type="text" class="form-control" required name="codigo_postal" v-model="cliente.codigo_postal" id="codigo_postal" aria-describedby="helpId" placeholder="Codigo Postal">
+                    <small id="helpId" class="form-text text-muted">Escribe el codgio Postal</small>
+                  </div>
 
-                    <div class="mb-3" v-if="provinciaTarget">
-                      <label for="municipio" class="form-label">Municipio:</label>
-                      <select v-model="cliente.municipio" required name="municipio" id="municipio" class="form-select" aria-label="Default select example">
-                        <option disabled selected>Elija un municipio</option>
-                        <option v-for="municipio in municipios" :key="municipio.id">{{municipio.municipio}}</option>
-                      </select>
-                    </div>
-                    
-                    <div class="mb-3">
-                      <label for="tipo" class="form-label">Tipo de cliente:</label>
+                  <div class="mb-3">
+                    <label for="municipio" class="form-label">Municipio:</label>
+                    <select v-model="municipioSeleccionado" required name="municipio" id="municipio" class="form-select" aria-label="Default select example">
+                      <option value="">Selecciona un municipio</option>
+                      <option v-for="municipio in municipios" :key="municipio.id" :value="municipio.id">
+                        {{ municipio.municipio }}
+                      </option>
+                    </select>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="tipo" class="form-label">Tipo de cliente:</label>
                       <select v-model="cliente.tipo" required name="tipo" id="tipo" class="form-select" aria-label="Default select example">
                         <option disabled selected>Elija un tipo</option>
                         <option value="Particular">Particular</option>
                         <option value="Empresa">Empresa</option>
-                        </select>
-                    </div>
+                      </select>
+                  </div>
 
-                    <div class="btn-group" role="group" aria-label="Button group name">
-                        <button type="submit" class="btn btn-success">Aceptar</button>
-                        <router-link :to="{name:'login'}" class="btn btn-warning">Cancelar</router-link>
-                    </div>
+                  <div class="btn-group" role="group" aria-label="Button group name">
+                    <button type="submit" class="btn btn-success">Aceptar</button>
+                    <router-link :to="{name:'login'}" class="btn btn-warning">Cancelar</router-link>
+                  </div>
+
                 </form>
             </div>
         </div>
@@ -73,51 +74,48 @@
 
 <script>
 export default {
-    data(){
-        return {
-            municipios: [],
-            provincias: [],
-            provinciaTarget: false,
-            cliente:{
+  data() {
+    return {
+      provincias: [],
+      municipios: [],
+      provinciaSeleccionada: '',
+      municipioSeleccionado: '',
+      cliente:{
 
-            }
-        }
+      }
+    }
+  },
+  created() {
+    this.cargarProvincias()
+  },
+  methods: {
+    cargarProvincias() {
+      fetch('http://localhost/provincias/')
+        .then(respuesta => respuesta.json())
+        .then(provincias => {
+          this.provincias = provincias
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
-    created: function () {
-        this.consultarProvincia();
+    cargarMunicipios() {
+      if (this.provinciaSeleccionada) {
+        fetch(`http://localhost/municipios/?consultarMunicipio=${this.provinciaSeleccionada}`)
+          .then(respuesta => respuesta.json())
+          .then(municipios => {
+            this.municipios = municipios
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      } else {
+        this.municipios = []
+      }
     },
-    methods:{
-      consultarProvincia() {
-            fetch('http://localhost/provincias/')
-                .then(respuesta => respuesta.json())
-                .then((datosRespuesta) => {
-
-                    console.log(datosRespuesta)
-                    this.provincias = []
-                    if (typeof datosRespuesta[0].success === 'undefined') {
-
-                        this.provincias = datosRespuesta;
-                    }
-                })
-                .catch(console.log)
-        },
-        consultarMunicipios(id) {
-            fetch('http://localhost/municipios/?consultarMunicipio=' + id)
-                .then(respuesta => respuesta.json())
-                .then((datosRespuesta) => {
-
-                    this.municipios = []
-                    if (typeof datosRespuesta[0].success === 'undefined') {
-
-                        this.municipios = datosRespuesta;
-                    }
-                })
-                .catch(console.log)
-        },
-        agregarRegistro(){
-            var datosEnviar={nombre: this.cliente.nombre, apellido: this.cliente.apellido, telefono: this.cliente.telefono, provincia: this.cliente.provincia,
-                codigo_postal: this.cliente.codigo_postal, municipio: this.cliente.municipio,  tipo: this.cliente.tipo}
-
+    agregarRegistro(){
+            var datosEnviar={nombre: this.cliente.nombre, apellido: this.cliente.apellido, telefono: this.cliente.telefono, provincia: this.provinciaSeleccionada,
+                codigo_postal: this.cliente.codigo_postal, municipio: this.municipioSeleccionado, tipo: this.cliente.tipo}
             fetch('http://localhost/user/?insertar=1', {
                 method:"POST",
                 body:JSON.stringify(datosEnviar)
@@ -129,6 +127,11 @@ export default {
                 }  
             }))
         }
-    } 
+  },
+  watch: {
+    provinciaSeleccionada() {
+      this.cargarMunicipios()
+    }
+  }
 }
 </script>
