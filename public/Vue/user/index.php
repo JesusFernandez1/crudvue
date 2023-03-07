43d9be7c0e3716740f9ae1bfb0bff2a9d00ec880
Jesus <jesus20102000@gmail.com>
2023-03-07 19:00:31 +0100

    commit

--- public/Vue/user/index.php
+++ public/Vue/user/index.php
@@ -6,209 +6,215 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // Conecta a la base de datos  con usuario, contraseña y nombre de la BD
-$servidor = "localhost"; $usuario = "jesusfernandez"; $contrasenia = "&xrK2g86"; $nombreBaseDatos = "jesusfernandez";
+$servidor = "localhost";
+$usuario = "jesusfernandez";
+$contrasenia = "&xrK2g86";
+$nombreBaseDatos = "jesusfernandez";
 $conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
 
 //------------------SECCION DE USUARIOS/CLIENTES-----------------------
 
 // Consulta datos y recepciona una clave para consultar dichos datos con dicha clave
-if (isset($_GET["consultar"])){
-    
-    $sqlUsuarios = mysqli_query($conexionBD,"SELECT * FROM cliente WHERE idcliente = '" . $_GET["consultar"] . "'");
-    if(mysqli_num_rows($sqlUsuarios) > 0){
-        $Usuarios = mysqli_fetch_all($sqlUsuarios,MYSQLI_ASSOC);
+if (isset($_GET["consultar"])) {
+
+    $sqlUsuarios = mysqli_query($conexionBD, "SELECT * FROM cliente WHERE idcliente = '" . $_GET["consultar"] . "'");
+    if (mysqli_num_rows($sqlUsuarios) > 0) {
+        $Usuarios = mysqli_fetch_all($sqlUsuarios, MYSQLI_ASSOC);
         echo json_encode($Usuarios);
         exit();
+    } else {
+        echo json_encode(false);
     }
-    else{  echo json_encode(false); }
 }
 
 if (isset($_GET["cliente1"]) && isset($_GET["cliente2"]) && isset($_GET["fechaInicio"]) && isset($_GET["fechaFin"]) && isset($_GET["estado"])) {
-    
+
     // Escapar los parámetros para evitar inyección de SQL
     $cliente1 = mysqli_real_escape_string($conexionBD, $_GET["cliente1"]);
     $cliente2 = mysqli_real_escape_string($conexionBD, $_GET["cliente2"]);
     $fechaInicio = mysqli_real_escape_string($conexionBD, $_GET["fechaInicio"]);
     $fechaFin = mysqli_real_escape_string($conexionBD, $_GET["fechaFin"]);
     $estado = mysqli_real_escape_string($conexionBD, $_GET["estado"]);
-  
-    // Construir la consulta SQL
-    $sql = "SELECT * FROM poliza WHERE estado = '$estado' AND cliente_idcliente IN ('$cliente1', '$cliente2') AND fecha BETWEEN '$fechaInicio' AND '$fechaFin'";
-    
+
+    if ($estado == "") {
+        $sql = "SELECT * FROM poliza WHERE cliente_idcliente BETWEEN LEAST('$cliente1', '$cliente2') AND GREATEST('$cliente1', '$cliente2') AND fecha BETWEEN '$fechaInicio' AND '$fechaFin'";
+    } else {
+        // Construir la consulta SQL
+        $sql = "SELECT * FROM poliza WHERE estado = '$estado' AND cliente_idcliente IN ('$cliente1', '$cliente2') AND fecha BETWEEN '$fechaInicio' AND '$fechaFin'";
+    }
+
     // Ejecutar la consulta y guardar los resultados en un array asociativo
     $resultado = $conexionBD->query($sql);
-  
+
     $polizas = array();
-  
+
     if ($resultado->num_rows > 0) {
-      while($fila = $resultado->fetch_assoc()) {
-        $polizas[] = $fila;
-      }
+        while ($fila = $resultado->fetch_assoc()) {
+            $polizas[] = $fila;
+        }
     }
-    
+
     echo json_encode($polizas);
     exit();
-  
-  }
+}
 
 if (isset($_GET["cliente1"]) && isset($_GET["cliente2"]) && isset($_GET["fechaInicio"]) && isset($_GET["fechaFin"])) {
-  
+
     // Escapar los parámetros para evitar inyección de SQL
     $cliente1 = mysqli_real_escape_string($conexionBD, $_GET["cliente1"]);
     $cliente2 = mysqli_real_escape_string($conexionBD, $_GET["cliente2"]);
     $fechaInicio = mysqli_real_escape_string($conexionBD, $_GET["fechaInicio"]);
     $fechaFin = mysqli_real_escape_string($conexionBD, $_GET["fechaFin"]);
-  
+
     // Construir la consulta SQL
-    $sql = "SELECT * FROM poliza WHERE cliente_idcliente IN ('$cliente1', '$cliente2') AND fecha BETWEEN '$fechaInicio' AND '$fechaFin'";
-  
+    $sql = "SELECT * FROM poliza WHERE cliente_idcliente BETWEEN LEAST('$cliente1', '$cliente2') AND GREATEST('$cliente1', '$cliente2') AND fecha BETWEEN '$fechaInicio' AND '$fechaFin'";
+
     // Ejecutar la consulta y guardar los resultados en un array asociativo
     $resultado = $conexionBD->query($sql);
-  
+
     $polizas = array();
-  
+
     if ($resultado->num_rows > 0) {
-      while($fila = $resultado->fetch_assoc()) {
-        $polizas[] = $fila;
-      }
+        while ($fila = $resultado->fetch_assoc()) {
+            $polizas[] = $fila;
+        }
     }
 
     echo json_encode($polizas);
     exit();
-  
-  }
+}
 
 //login enviando los datos del formulario
-if (isset($_GET["login"])){
+if (isset($_GET["login"])) {
     $data = json_decode(file_get_contents("php://input"));
-    $correo=$data->correo;
-    $pass=$data->pass;
-    $sqlUsuarios = mysqli_query($conexionBD,"SELECT * FROM usuario WHERE correo = '". $correo . "' AND pass = '" . $pass . "'");
-    if(mysqli_num_rows($sqlUsuarios) > 0){
-        $Usuarios = mysqli_fetch_all($sqlUsuarios,MYSQLI_ASSOC);
+    $correo = $data->correo;
+    $pass = $data->pass;
+    $sqlUsuarios = mysqli_query($conexionBD, "SELECT * FROM usuario WHERE correo = '" . $correo . "' AND pass = '" . $pass . "'");
+    if (mysqli_num_rows($sqlUsuarios) > 0) {
+        $Usuarios = mysqli_fetch_all($sqlUsuarios, MYSQLI_ASSOC);
         echo json_encode(true);
         exit();
+    } else {
+        echo json_encode(false);
     }
-    else{  echo json_encode(false); }
 }
 
 //Inserta un nuevo registro y recepciona en método post los datos de correo y contraseña
-if(isset($_GET["insertar"])){
+if (isset($_GET["insertar"])) {
     $data = json_decode(file_get_contents("php://input"));
-    $nombre=$data->nombre;
-    $apellido=$data->apellido;
-    $telefono=$data->telefono;
-    $provincia=$data->provincia;
-    $codigo_postal=$data->codigo_postal;
-    $municipio=$data->municipio;
-    $tipo=$data->tipo;
+    $nombre = $data->nombre;
+    $apellido = $data->apellido;
+    $telefono = $data->telefono;
+    $provincia = $data->provincia;
+    $codigo_postal = $data->codigo_postal;
+    $municipio = $data->municipio;
+    $tipo = $data->tipo;
 
     $resultadoProvincia = mysqli_query($conexionBD, "SELECT provincia FROM provincias WHERE id = $provincia");
     $provinciaNombre = mysqli_fetch_assoc($resultadoProvincia)['provincia'];
     $resultadoMunicipio = mysqli_query($conexionBD, "SELECT municipio FROM municipios WHERE id = $municipio");
     $municipioNombre = mysqli_fetch_assoc($resultadoMunicipio)['municipio'];
 
-        if(validarNombreApellido($nombre) == 1 && validarNombreApellido($apellido) == 1 && comprobarCodigo($codigo_postal) == 1 &&  validarTelefono($telefono) == 1){
-        $sqlUsuarios = mysqli_query($conexionBD,"INSERT INTO cliente(nombre, apellido, telefono, provincia, codigo_postal, municipio, tipo) VALUES('$nombre', '$apellido', '$telefono', '$provinciaNombre', '$codigo_postal', '$municipioNombre', '$tipo')");
+    if (validarNombreApellido($nombre) == 1 && validarNombreApellido($apellido) == 1 && comprobarCodigo($codigo_postal) == 1 &&  validarTelefono($telefono) == 1) {
+        $sqlUsuarios = mysqli_query($conexionBD, "INSERT INTO cliente(nombre, apellido, telefono, provincia, codigo_postal, municipio, tipo) VALUES('$nombre', '$apellido', '$telefono', '$provinciaNombre', '$codigo_postal', '$municipioNombre', '$tipo')");
 
-        echo json_encode(true);    
+        echo json_encode(true);
         exit();
     } else {
-        echo json_encode(false);    
+        echo json_encode(false);
         exit();
     }
-   
 }
 
 // Actualiza datos pero recepciona datos de nombre, correo y una clave para realizar la actualización
-if(isset($_GET["actualizar"])){
-    
+if (isset($_GET["actualizar"])) {
+
     $data = json_decode(file_get_contents("php://input"));
 
-    $idcliente=(isset($data->idcliente))?$data->idcliente:$_GET["actualizar"];
-    $nombre=$data->nombre;
-    $apellido=$data->apellido;
-    $telefono=$data->telefono;
-    $provincia=$data->provincia;
-    $codigo_postal=$data->codigo_postal;
-    $municipio=$data->municipio;
-    $tipo=$data->tipo;
+    $idcliente = (isset($data->idcliente)) ? $data->idcliente : $_GET["actualizar"];
+    $nombre = $data->nombre;
+    $apellido = $data->apellido;
+    $telefono = $data->telefono;
+    $provincia = $data->provincia;
+    $codigo_postal = $data->codigo_postal;
+    $municipio = $data->municipio;
+    $tipo = $data->tipo;
 
     if ($provincia == "") {
-        
+
         $resultadoProvincia = mysqli_query($conexionBD, "SELECT provincia FROM cliente WHERE idcliente = $idcliente");
         $provinciaNombre = mysqli_fetch_assoc($resultadoProvincia)['provincia'];
         $resultadoMunicipio = mysqli_query($conexionBD, "SELECT municipio FROM cliente WHERE idcliente = $idcliente");
         $municipioNombre = mysqli_fetch_assoc($resultadoMunicipio)['municipio'];
-
     } else {
 
         $resultadoProvincia = mysqli_query($conexionBD, "SELECT provincia FROM provincias WHERE id = $provincia");
         $provinciaNombre = mysqli_fetch_assoc($resultadoProvincia)['provincia'];
         $resultadoMunicipio = mysqli_query($conexionBD, "SELECT municipio FROM municipios WHERE id = $municipio");
         $municipioNombre = mysqli_fetch_assoc($resultadoMunicipio)['municipio'];
-
     }
 
-    $sqlUsuarios = mysqli_query($conexionBD,"UPDATE cliente SET nombre = '$nombre', apellido = '$apellido', telefono = '$telefono', provincia = '$provinciaNombre', codigo_postal = '$codigo_postal', municipio = '$municipioNombre', tipo = '$tipo' WHERE idcliente = '$idcliente'");
+    $sqlUsuarios = mysqli_query($conexionBD, "UPDATE cliente SET nombre = '$nombre', apellido = '$apellido', telefono = '$telefono', provincia = '$provinciaNombre', codigo_postal = '$codigo_postal', municipio = '$municipioNombre', tipo = '$tipo' WHERE idcliente = '$idcliente'");
     echo json_encode(true);
     exit();
 }
 
 //borrar pero se le debe de enviar una clave ( para borrado )
-if (isset($_GET["borrar"])){
-    mysqli_query($conexionBD,"DELETE FROM poliza WHERE cliente_idcliente = '".$_GET["borrar"] . "'");
-    $sqlUsuarios = mysqli_query($conexionBD,"DELETE FROM cliente WHERE idcliente = '".$_GET["borrar"] . "'");
-    if($sqlUsuarios){
+if (isset($_GET["borrar"])) {
+    mysqli_query($conexionBD, "DELETE FROM poliza WHERE cliente_idcliente = '" . $_GET["borrar"] . "'");
+    $sqlUsuarios = mysqli_query($conexionBD, "DELETE FROM cliente WHERE idcliente = '" . $_GET["borrar"] . "'");
+    if ($sqlUsuarios) {
         echo json_encode(true);
         exit();
+    } else {
+        echo json_encode(false);
     }
-    else{  echo json_encode(false); }
 }
 
 // Consulta todos los registros de la tabla empleados
-$sqlUsuarios = mysqli_query($conexionBD,"SELECT * FROM cliente ");
-if(mysqli_num_rows($sqlUsuarios) > 0){
-    $Usuarios = mysqli_fetch_all($sqlUsuarios,MYSQLI_ASSOC);
+$sqlUsuarios = mysqli_query($conexionBD, "SELECT * FROM cliente ");
+if (mysqli_num_rows($sqlUsuarios) > 0) {
+    $Usuarios = mysqli_fetch_all($sqlUsuarios, MYSQLI_ASSOC);
     echo json_encode($Usuarios);
+} else {
+    echo json_encode([false]);
 }
-else{ echo json_encode([false]); }
 
 
 function validarNombreApellido($nombre)
-    {
-        $pattern = "/^[a-z]+$/i";
+{
+    $pattern = "/^[a-z]+$/i";
 
-        if ((preg_match($pattern, $nombre))) {
+    if ((preg_match($pattern, $nombre))) {
 
-            return 1;
-        } else {
+        return 1;
+    } else {
 
-            return 0;
-        }
+        return 0;
     }
+}
 
-    function comprobarCodigo($codigo) {
+function comprobarCodigo($codigo)
+{
 
-        if (preg_match('/^[0-9]{5,5}$/', $codigo)) {
-            return 1;
-        } else {
-            return 0;
-        }
-        
+    if (preg_match('/^[0-9]{5,5}$/', $codigo)) {
+        return 1;
+    } else {
+        return 0;
     }
+}
 
-    function validarTelefono($numero)
-    {
-        $reg = '/(\+34|0034|34)?[ -]*(6|7|8|9)[ -]*([0-9][ -]*){8}/';
-        if (preg_match($reg, $numero)) {
+function validarTelefono($numero)
+{
+    $reg = '/(\+34|0034|34)?[ -]*(6|7|8|9)[ -]*([0-9][ -]*){8}/';
+    if (preg_match($reg, $numero)) {
 
-            return 1;
-        } else {
-            
-            return 0;
-        }
+        return 1;
+    } else {
+
+        return 0;
     }
+}
 
 
     //------------------SECCION DE POLIZAS-----------------------
--- src/components/Listar.vue
+++ src/components/Listar.vue
@@ -1,55 +1,72 @@
 <template>
-    <nav>
-    <router-link to="/listar">Home</router-link> |
-    <router-link to="/VerPolizas">Ver cuotas</router-link> |
-    <router-link to="/filtrar">Filtrar</router-link>
-    </nav>
-    <div>
-        <div class="card">
-            <div class="card-header">
-                Clientes
-            </div>
-            <div class="card-body">
-                <table id="listado" class="table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nombre</th>
-                            <th>Apellido</th>
-                            <th>Telefono</th>
-                            <th>Localidad</th>
-                            <th>Codigo postal</th>
-                            <th>Tipo</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="cliente in clientes" :key="cliente.idcliente">
-                            <td>{{ cliente.idcliente }}</td>
-                            <td>{{ cliente.nombre }}</td>
-                            <td>{{ cliente.apellido }}</td>
-                            <td>{{ cliente.telefono }}</td>
-                            <td>{{ cliente.provincia }}</td>
-                            <td>{{ cliente.codigo_postal }}</td>
-                            <td>{{ cliente.tipo }}</td>
-                            <td>
-                                <div class="btn-group" role="group" aria-label="">
-
-                                   <router-link :to="{name:'polizas',params:{id:cliente.idcliente}}"  class="btn btn-info">Ver detalles</router-link>
-
-                                   <router-link :to="{name:'crearPoliza',params:{id:cliente.idcliente}}"  class="btn btn-success">Crear poliza</router-link>
-
-                                    <router-link :to="{name:'modificar',params:{id:cliente.idcliente}}"  class="btn btn-warning">Modificar</router-link>
-                                        
-                                    <router-link :to="{name:'confirmarEliminar',params:{id:cliente.idcliente}}"  class="btn btn-danger">Borrar</router-link>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/listar">Mi sitio web</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/listar">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/VerPolizas">Ver cuotas</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/filtrar">Filtrar</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link class="nav-link btn btn-danger" to="/">Logout</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
+    <div class="container mt-4">
+    <div class="card">
+      <div class="card-header bg-primary text-white">
+        Clientes
+      </div>
+      <div class="card-body">
+        <table id="listado" class="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Teléfono</th>
+              <th>Localidad</th>
+              <th>Código Postal</th>
+              <th>Tipo</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="cliente in clientes" :key="cliente.idcliente">
+              <td>{{ cliente.idcliente }}</td>
+              <td>{{ cliente.nombre }}</td>
+              <td>{{ cliente.apellido }}</td>
+              <td>{{ cliente.telefono }}</td>
+              <td>{{ cliente.provincia }}</td>
+              <td>{{ cliente.codigo_postal }}</td>
+              <td>{{ cliente.tipo }}</td>
+              <td>
+                <div class="btn-group" role="group">
+                  <router-link :to="{name:'polizas',params:{id:cliente.idcliente}}" class="btn btn-info">Ver detalles</router-link>
+                  <router-link :to="{name:'crearPoliza',params:{id:cliente.idcliente}}" class="btn btn-success">Crear póliza</router-link>
+                  <router-link :to="{name:'modificar',params:{id:cliente.idcliente}}" class="btn btn-warning">Modificar</router-link>
+                  <router-link :to="{name:'confirmarEliminar',params:{id:cliente.idcliente}}" class="btn btn-danger">Borrar</router-link>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
